1c25128e5d5cc97514b087efb803b35829d0cdec
Admin HV <admin@dongphuchv.com>
2026-03-29 12:55:51 +0700

    fix: add task_schedule_active_teams to noIdTables to prevent RETURNING id error

--- db/pool.js
+++ db/pool.js
@@ -42,7 +42,7 @@ const database = {
 
         // Auto-add RETURNING id for INSERT statements (if not already present)
         // Skip for tables that don't have 'id' column (e.g., app_config uses 'key' as PK)
-        const noIdTables = ['app_config'];
+        const noIdTables = ['app_config', 'task_schedule_active_teams'];
         const hasNoId = noIdTables.some(t => new RegExp('INTO\\s+' + t, 'i').test(finalSql));
         if (/^\s*INSERT\s/i.test(finalSql) && !/RETURNING/i.test(finalSql) && !hasNoId) {
             finalSql = finalSql.replace(/;?\s*$/, ' RETURNING id');
