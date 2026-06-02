2193e1f88bfb4640a2aa5ce82eb9c348805f1d14
Admin HV <admin@dongphuchv.com>
2026-06-02 17:57:28 +0700

    feat: group same multi_cut_group_id together

--- routes/bophancat.js
+++ routes/bophancat.js
@@ -335,7 +335,7 @@ module.exports = async function(fastify) {
             ) lh ON true
             LEFT JOIN users lh_user ON lh.performed_by = lh_user.id
             ${where}
-            ORDER BY cr.is_cutting DESC, o.order_code ASC NULLS LAST, cr.product_name ASC, cr.created_at DESC
+            ORDER BY cr.is_cutting DESC, COALESCE(cr.multi_cut_group_id, 'ZZZ') ASC, o.order_code ASC NULLS LAST, cr.product_name ASC, cr.created_at DESC
         `, params);
 
         return { records };
