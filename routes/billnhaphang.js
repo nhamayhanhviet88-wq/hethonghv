06fee35abea19c5eec4e122df1425136fb2187a8
Admin HV <admin@dongphuchv.com>
2026-06-30 17:42:23 +0700

    feat: use cutting target ratio of Ao category for original import prices

--- routes/billnhaphang.js
+++ routes/billnhaphang.js
@@ -2052,7 +2052,11 @@ module.exports = async function(fastify) {
                 kfm.name AS fabric_material_name,
                 kfc.color_name AS fabric_color_name,
                 wh.name AS warehouse_name,
-                kfm.target_cut_ratio AS fabric_cut_ratio
+                COALESCE(
+                    (SELECT target_ratio FROM kv_material_cutting_targets WHERE material_id = kfm.id AND cutting_category = 'Áo'),
+                    kfm.target_cut_ratio,
+                    0
+                ) AS fabric_cut_ratio
             FROM approved_import_prices ap
             LEFT JOIN import_sources s ON ap.source_id = s.id
             LEFT JOIN kv_fabric_colors kfc ON ap.fabric_color_id = kfc.id
