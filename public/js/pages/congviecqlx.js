35247c0dc79c48d6b37a2d352c722f43abcbaacf
Admin HV <admin@dongphuchv.com>
2026-07-23 09:41:04 +0700

    Fix hardcoded old version tag of chuanbiqlx.js in congviecqlx.js

--- public/js/pages/congviecqlx.js
+++ public/js/pages/congviecqlx.js
@@ -91,7 +91,7 @@ function _cvqlxRenderActiveTab() {
             break;
 
         case 'chuanbi':
-            _loadScript('/js/pages/chuanbiqlx.js?v=20260722_qlx_reminders_v29').then(() => {
+            _loadScript('/js/pages/chuanbiqlx.js?v=20260723_call_fab_cat_reminders_v3').then(() => {
                 if (typeof renderQuanlyxuongqlxPage === 'function') {
                     renderQuanlyxuongqlxPage(container);
                 } else {
