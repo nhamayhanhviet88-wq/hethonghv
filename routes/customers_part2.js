c155dbe0a09d03c370f4ac33db1145ae6bdc3b2b
Admin HV <admin@dongphuchv.com>
2026-07-25 15:05:17 +0700

    fix: SQL parameter placeholders in dat_coc lock payment query

--- routes/customers_part2.js
+++ routes/customers_part2.js
@@ -1230,15 +1230,15 @@ module.exports = function(fastify, db, getManagedDeptIds) {
                 UPDATE payment_records SET
                     payment_type = 'dat_coc',
                     handover_status = 'thu_quy_nhan',
-                    customer_name = $1,
-                    customer_phone = $2,
-                    cskh_user_id = $3,
-                    order_tt_coc = $5,
-                    locked_by = $3,
+                    customer_name = ?,
+                    customer_phone = ?,
+                    cskh_user_id = ?,
+                    order_tt_coc = ?,
+                    locked_by = ?,
                     locked_at = NOW(),
                     updated_at = NOW()
-                WHERE id = $4
-            `, [customer.customer_name, customer.phone, request.user.id, prId, targetOrderCode]);
+                WHERE id = ?
+            `, [customer.customer_name, customer.phone, request.user.id, targetOrderCode, request.user.id, prId]);
             if (lockResult.changes === 0) {
                 return reply.code(409).send({ error: 'Mã tiền này đã được nhận bởi người khác!' });
             }
