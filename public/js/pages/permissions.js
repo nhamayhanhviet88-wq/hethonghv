17aa7fea67e20c428f87589be1d38a5c6eb8dc7d
Admin HV <admin@dongphuchv.com>
2026-08-03 23:38:16 +0700

    feat: rename menu label KPI Marketing to KPI Marketing Ads

--- public/js/pages/permissions.js
+++ public/js/pages/permissions.js
@@ -7,7 +7,7 @@ const PERM_FEATURES = [
     { key: 'dashboard_kdoanh', label: 'Dashboard P.Kinh Doanh', perms: ['view'], section: 'TỔNG QUAN' },
     { key: 'kpi_kdoanh', label: 'KPI P.Kinh Doanh', perms: ['view'], section: 'TỔNG QUAN' },
     { key: 'kpi_sale', label: 'KPI P.Sale', perms: ['view'], section: 'TỔNG QUAN' },
-    { key: 'kpi_marketing', label: 'KPI Marketing', perms: ['view','edit'], section: 'TỔNG QUAN' },
+    { key: 'kpi_marketing', label: 'KPI Marketing Ads', perms: ['view','edit'], section: 'TỔNG QUAN' },
     { key: 'ngan_sach_mkt', label: 'Ngân Sách Marketing', perms: ['view','create','edit','delete'], section: 'TỔNG QUAN' },
     { key: 'tim_kiem_kh', label: 'Tìm Kiếm Khách Hàng', perms: ['view'], section: 'TỔNG QUAN' },
 
